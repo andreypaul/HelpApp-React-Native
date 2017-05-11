45a1749bda83637330c3e718e4eba23a5484cc8e
Andrew Polyakov <Andrey@MacBook-Pro-Andrew.local>
2017-05-11 18:15:43 +0300

    Change index.ios.js

--- index.ios.js
+++ index.ios.js
@@ -1,8 +1,6 @@
 import React, { Component } from 'react';
 import {
     AppRegistry,
-    Text,
-    View,
     StyleSheet,
 } from 'react-native';
 import {Scene, Router, Actions} from 'react-native-router-flux';

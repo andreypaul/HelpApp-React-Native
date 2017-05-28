d642ca506f92c6e86d76e1dbd1d9ca903b915e76
andreypaul <andrey-polyakov@mail.ru>
2017-05-29 01:00:28 +0300

    first commit

--- ios/build/Build/Intermediates/React.build/Debug-iphonesimulator/yoga.build/Objects-normal/x86_64/YGNodeList.d
+++ ios/build/Build/Intermediates/React.build/Debug-iphonesimulator/yoga.build/Objects-normal/x86_64/YGNodeList.d
@@ -1,6 +1,3 @@
 dependencies: \
-  /Users/Andrey/WebstormProjects/HelpMeApp/node_modules/react-native/ReactCommon/yoga/yoga/YGNodeList.c \
-  /Users/Andrey/WebstormProjects/HelpMeApp/node_modules/react-native/ReactCommon/yoga/yoga/YGNodeList.h \
-  /Users/Andrey/WebstormProjects/HelpMeApp/node_modules/react-native/ReactCommon/yoga/yoga/YGMacros.h \
-  /Users/Andrey/WebstormProjects/HelpMeApp/node_modules/react-native/ReactCommon/yoga/yoga/Yoga.h \
-  /Users/Andrey/WebstormProjects/HelpMeApp/node_modules/react-native/ReactCommon/yoga/yoga/YGEnums.h
+  /Users/Andrey/WebstormProjects/HelpApp-React-Native-test/node_modules/react-native/ReactCommon/yoga/yoga/YGNodeList.c \
+  /Users/Andrey/WebstormProjects/HelpApp-React-Native-test/node_modules/react-native/ReactCommon/yoga/yoga/YGNodeList.h
